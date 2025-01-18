19eedb8cb921eb08be717e913974a595330e4b7e
jubayer <jubayerahmed46@gmail.com>
2025-01-18 08:42:09 +0600

    update gat article using aggregate

--- src/routes/articleRoute.js
+++ src/routes/articleRoute.js
@@ -29,4 +29,58 @@ router.post("/", async (req, res) => {
   res.send(result);
 });
 
+router.get("/", async (req, res) => {
+  const articleCollection = await articlesCollection();
+  console.log("start");
+
+  /**
+   * - get only published articles
+   * - convert user in to objectId for getting user information from users collection
+   * - fetch user data using local( mean article associeted userId)
+   * - define which field match with users collection
+   * - using "as" keyword create a new array and push all the value of user object
+   * - at last convert array to an object
+   * */
+  const result = await articleCollection
+    .aggregate([
+      {
+        $match: {
+          status: "published",
+        },
+      },
+      {
+        $addFields: {
+          authorId: { $toObjectId: "$authorInfo.userId" },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "authorId",
+          foreignField: "_id",
+          as: "authorInfo",
+        },
+      },
+
+      {
+        $project: {
+          title: 1,
+          description: 1,
+          creationTime: 1,
+          publisher: 1,
+          tags: 1,
+          thumbnail: 1,
+          "authorInfo.fullName": 1,
+          "authorInfo.email": 1,
+          "authorInfo.profilePhoto": 1,
+        },
+      },
+      {
+        $unwind: "$authorInfo",
+      },
+    ])
+    .toArray();
+
+  res.send(result);
+});
 module.exports = router;
